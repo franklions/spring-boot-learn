e3dc67581e263616a8e6246b03fbcc70e371ebbe
flsh <fenglianshuang@jiuan.com>
2017-10-25 09:18:21 +0800

    添加自动生成api文档

--- elasticsearch-client-demo/src/main/java/elasticsearch/client/demo/EsRunApplicaton.java
+++ elasticsearch-client-demo/src/main/java/elasticsearch/client/demo/EsRunApplicaton.java
@@ -18,15 +18,15 @@ public class EsRunApplicaton {
     public static void main(String[] args) {
         EmployeeService employeeService = new EmployeeService();
 
-//        // 添加员工信息
-//        employeeService.addEmployee(new Employee(1,"John"
-//                ,"Smith",25,"I love to go rock climbing", Arrays.asList("sports","music")));
-//
-//        employeeService.addEmployee(new Employee(2,"Jane"
-//                ,"Smith",32,"I like to collect rock albums", Arrays.asList("music")));
-//
-//        employeeService.addEmployee(new Employee(3,"Douglas"
-//                ,"Fir",35,"I like to build cabinets", Arrays.asList("forestry")));
+        // 添加员工信息
+        employeeService.addEmployee(new Employee(1,"John"
+                ,"Smith",25,"I love to go rock climbing", Arrays.asList("sports","music")));
+
+        employeeService.addEmployee(new Employee(2,"Jane"
+                ,"Smith",32,"I like to collect rock albums", Arrays.asList("music")));
+
+        employeeService.addEmployee(new Employee(3,"Douglas"
+                ,"Fir",35,"I like to build cabinets", Arrays.asList("forestry")));
 
         //查询员工1的信息
         Employee employee1 = employeeService.getEmployee(1);
