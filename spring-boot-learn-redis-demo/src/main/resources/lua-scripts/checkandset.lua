902e171b59b14db0f32baeb3a82c6cfb5b9c531e
flsh <fenglianshuang@jiuan.com>
2018-03-07 17:27:44 +0800

    修改lua脚本

--- spring-boot-learn-redis-demo/src/main/resources/lua-scripts/checkandset.lua
+++ spring-boot-learn-redis-demo/src/main/resources/lua-scripts/checkandset.lua
@@ -1,8 +1,5 @@
  -- checkandset.lua local
 local current = redis.call('GET', KEYS[1])
- print(KEYS[1])
- print(ARGV[1])
- print("abcdefs")
  if current == ARGV[1]
  then
      redis.call('SET', KEYS[1], ARGV[2])
